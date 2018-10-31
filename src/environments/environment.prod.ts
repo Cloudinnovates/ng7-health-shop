b29a222b7ad0ef3c9fa5226898bfc4994e49d37a
NilsNaegele <nils.holger.naegele@gmail.com>
2018-10-31 08:47:16 +0100

    login

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCOm_H-wknx6o7okSuncUcLzJ66vWiYhA0',
+    authDomain: 'ng7-health-shop-ad3d9.firebaseapp.com',
+    databaseURL: 'https://ng7-health-shop-ad3d9.firebaseio.com',
+    projectId: 'ng7-health-shop-ad3d9',
+    storageBucket: 'ng7-health-shop-ad3d9.appspot.com',
+    messagingSenderId: '404934521343'
+  }
 };
